e28df3270f68f6639e4502bb27b8f515af8cb8ba
Anderson Brischiliari <88595771+Andibris@users.noreply.github.com>
2022-11-28 02:12:02 -0300

    primeira versão do desafio

---
+++ sem_fetch_v1/script.js
@@ -0,0 +1,59 @@
+// window.onload = function(){
+
+//     const botao = document.getElementById('btn');
+//     botao.addEventListener('click',function(){
+        
+        
+        
+//         function passaParam(url, body){
+//             console.log(body);    
+//         }
+
+//         function criaUrl(){
+//             preventDefault()
+//             let url = "http://www.google.com/search?q="
+//             let valor = document.getElementById('input');
+
+//             console.log(url);
+//             console.log(valor.value);
+//             body = {
+//                 "param": valor.value
+//             }
+
+//             passaParam(url,body)
+
+//         }
+
+
+
+
+        
+        
+//     });
+
+// }
+
+
+// function fazPost(link){
+//     let request = new XMLHttpRequest()
+//     request.open("POST", link, true)
+//     request.setRequestHeader("Content-type","application/json")
+    
+
+//     request.onload = function(){
+//         console.log(this.responseText)
+//     }
+
+//     return request.responseText
+// }
+
+// function criaValue(){
+//     event.preventDefault()
+//     let url = "http://www.google.com/search?q="
+
+//     let link = url + document.getElementById("input").value
+//     console.log(link)
+
+//     fazPost(link) 
+// }
+
